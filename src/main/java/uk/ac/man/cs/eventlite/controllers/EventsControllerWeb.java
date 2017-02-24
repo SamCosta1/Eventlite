cecbdd75ad7b00267ce0337e88b905920f08ef1f
Sam <samuel.dacosta@student.manchester.ac.uk>
2017-02-24 22:02:51 +0000

    Updated the view and controller to enable searching
    Created a search object, the intent is that as we add more search criteria (e.g date/time filters) they should be able to be submitted through the same form. This way all the info can be brought together in the search object. 
    
    The search() method can then be expanded to perform more complex searches with multiple filters depending on user's input

--- src/main/java/uk/ac/man/cs/eventlite/controllers/EventsControllerWeb.java
+++ src/main/java/uk/ac/man/cs/eventlite/controllers/EventsControllerWeb.java
@@ -4,12 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import uk.ac.man.cs.eventlite.dao.EventService;
-import uk.ac.man.cs.eventlite.dao.VenueService;
-import uk.ac.man.cs.eventlite.entities.Event;
+import uk.ac.man.cs.eventlite.dao.Search;
 
 @Controller
 @RequestMapping("/events")
@@ -18,14 +19,20 @@ public class EventsControllerWeb {
 	@Autowired
 	private EventService eventService;
 
-	@Autowired
-	private VenueService venueService;
-
 	@RequestMapping(method = RequestMethod.GET, produces = { MediaType.TEXT_HTML_VALUE })
 	public String getAllEvents(Model model) {
 
 		model.addAttribute("events", eventService.findAll());
 		return "events/index";
 	}
+	
+	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE, produces = { MediaType.TEXT_HTML_VALUE })
+	public String filter(@ModelAttribute("search") Search searchCriterion, BindingResult result, Model model) {
+	
+		model.addAttribute("events", searchCriterion.search(eventService));
+		return "events/index";
+	}
+	
+
 
 }
