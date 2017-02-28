a79b23f941aa22398af3112722b5d9236e0fcbbc
Lucian-Iulian Ghimfus <lucian-iulian.ghimfus@student.manchester.ac.uk>
2017-02-28 14:49:04 +0000

    Added tests for event deletion via WEB/API.

--- src/test/java/uk/ac/man/cs/eventlite/controllers/EventsControllerWebTest.java
+++ src/test/java/uk/ac/man/cs/eventlite/controllers/EventsControllerWebTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import uk.ac.man.cs.eventlite.TestParent;
 
@@ -22,5 +23,12 @@ public class EventsControllerWebTest extends TestParent {
 	public void testGetAllEvents() throws Exception {
 		mvc.perform(get("/events").accept(MediaType.TEXT_HTML)).andExpect(status().isOk())
 				.andExpect(view().name("events/index"));
+	}
+	
+	@Test
+	public void testDeleteEvent() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.post("/events/1/delete")
+		.contentType(MediaType.APPLICATION_FORM_URLENCODED).accept(MediaType.TEXT_HTML)).andExpect(status().isFound())
+		.andExpect(view().name("redirect:/events"));
 	}
 }
