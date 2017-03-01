8841f0f27326ce8bfae95bd52294f536f941984b
Kaspar Matas <kaspar.matas@student.manchester.ac.uk>
2017-03-01 21:22:01 +0200

    Added panels and bootstrap grid to event view page
    Started the base of the event view page UI by using bootstrap grid and
    panels and made sure that the description field of and event can store
    long texts.

--- src/main/java/uk/ac/man/cs/eventlite/entities/Event.java
+++ src/main/java/uk/ac/man/cs/eventlite/entities/Event.java
@@ -2,9 +2,11 @@ package uk.ac.man.cs.eventlite.entities;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -24,6 +26,8 @@ public class Event {
 
 	private String name;
 	
+	@Lob
+	@Column( length = 100000 )
 	private String description;
 
 	@ManyToOne
