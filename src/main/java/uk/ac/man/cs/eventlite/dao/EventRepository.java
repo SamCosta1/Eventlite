4d747b3e6fdee20092114c2e1844657384efe0d3
Kaspar Matas <kaspar.matas@student.manchester.ac.uk>
2017-03-16 21:18:11 +0200

    Implementation of the new venue view page
    Implemented the new venue view page and added a find all method to event
    service in order to find all the events which are associated with the
    given venue.

--- src/main/java/uk/ac/man/cs/eventlite/dao/EventRepository.java
+++ src/main/java/uk/ac/man/cs/eventlite/dao/EventRepository.java
@@ -3,10 +3,12 @@ package uk.ac.man.cs.eventlite.dao;
 import org.springframework.data.repository.CrudRepository;
 
 import uk.ac.man.cs.eventlite.entities.Event;
+import uk.ac.man.cs.eventlite.entities.Venue;
 
 public interface EventRepository extends CrudRepository<Event, Long> {
 	
 	Iterable<Event> findAllByOrderByDateAscNameAsc();
 	Event findById(long id);
 	Iterable<Event> findByNameContainingIgnoreCaseOrderByDateAscNameAsc(String name);
+	Iterable<Event> findAllByVenue(Venue venue);
 }
