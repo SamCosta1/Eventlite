7a053c30c2aadb8a3a3175a5f479a3244dfaafb8
Sam <samuel.dacosta@student.manchester.ac.uk>
2017-04-07 22:03:49 +0100

    Split the list views into appropriate seperate files
    Main bulk here is finishing the list views and tidiying the code, also restyled the venues pages and the register and login ones

--- src/main/resources/static/js/site.js
+++ src/main/resources/static/js/site.js
@@ -1,20 +1,19 @@
-
 $(document).ready(function() {
 
-$(".buttons-group").on("click",function (e) {
-console.log("clicked", $(e.target).data("value"));
-    switch ($(e.target).data("value")) {
-        case "listview":
-	    	$("#list").css("display", "block");
-	    	$("#map").css("display","none");
-            break;
-        case "mapview":
-       		$("#list").css("display","none");
-       		$("#map").css("display", "block");
-       		google.maps.event.trigger(map, 'resize');
-            break;
-    }
+	$(".buttons-group").on("click",function (e) {
+		
+	    switch ($(e.target).data("value")) {
+	        case "listview":
+		    	$("#list").css("display", "block");
+		    	$("#map").css("display","none");
+	            break;
+	        case "mapview":
+	       		$("#list").css("display","none");
+	       		$("#map").css("display", "block");
+	       		google.maps.event.trigger(map, 'resize');
+	            break;
+	    }
+	});
 });
- });
  
  
