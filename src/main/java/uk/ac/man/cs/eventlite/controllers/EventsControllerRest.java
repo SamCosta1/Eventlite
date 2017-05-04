d4fdf62726caf9b5f07dc0219ce6b9acd45e83ae
Kaspar Matas <kaspar.matas@student.manchester.ac.uk>
2017-05-04 23:00:37 +0300

    Made sure that a correct Json is returned from user events page
    Changed the controller type so that correctly formated Json could be
    returned from the user events page rest controller

--- src/main/java/uk/ac/man/cs/eventlite/controllers/EventsControllerRest.java
+++ src/main/java/uk/ac/man/cs/eventlite/controllers/EventsControllerRest.java
@@ -5,18 +5,18 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
 
 import uk.ac.man.cs.eventlite.dao.EventService;
 import uk.ac.man.cs.eventlite.dao.SearchEvents;
 import uk.ac.man.cs.eventlite.entities.Event;
 
-@RestController
+@Controller
 @RequestMapping("/events")
 public class EventsControllerRest {
 
