331d1ae75f7066c6f86c7d9efe7ea5f48133fa49
Lucian-Iulian Ghimfus <lucian-iulian.ghimfus@student.manchester.ac.uk>
2017-05-04 22:09:56 +0100

    Events rest controller returns correct path
    Modified the events controller in order to return the correct path. 
    
    The unit test is passing now

--- src/main/java/uk/ac/man/cs/eventlite/controllers/EventsControllerRest.java
+++ src/main/java/uk/ac/man/cs/eventlite/controllers/EventsControllerRest.java
@@ -6,12 +6,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import uk.ac.man.cs.eventlite.dao.EventService;
 import uk.ac.man.cs.eventlite.dao.SearchEvents;
@@ -25,8 +28,11 @@ public class EventsControllerRest {
 	private EventService eventService;
 
 	@RequestMapping(method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE })
-	public String getAllEvents() {
-
+	public String getAllEvents(Model model, UriComponentsBuilder b) {
+		
+		UriComponents link = b.path("/events").build();
+		model.addAttribute("self_link", link.toUri());
+		
 		return "events/index";
 	}
 	
