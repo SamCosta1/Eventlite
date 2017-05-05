7ae883e204156bb51549e80e73191cefc8dae027
Nikola <nikola.grunchevski@student.manchester.ac.uk>
2017-05-05 12:06:02 +0100

    REST for homepage implemented

--- src/test/java/uk/ac/man/cs/eventlite/HomePageTest.java
+++ src/test/java/uk/ac/man/cs/eventlite/HomePageTest.java
@@ -1,7 +1,7 @@
 package uk.ac.man.cs.eventlite;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.Ignore;
 import org.junit.Test;
@@ -9,6 +9,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.core.StringContains.containsString;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import uk.ac.man.cs.eventlite.TestParent;
 
 @AutoConfigureMockMvc
 public class HomePageTest extends TestParent {
@@ -21,9 +38,14 @@ public class HomePageTest extends TestParent {
 		mvc.perform(get("/").accept(MediaType.TEXT_HTML)).andExpect(status().isOk());
 	}
 	
-	@Ignore // Ignoring since we're not doing anything with the rest controllers yet
 	@Test
 	public void getJsonRoot() throws Exception {
-		mvc.perform(get("/").accept(MediaType.APPLICATION_JSON)).andExpect(status().isNotFound());
+		mvc.perform(get("/").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+		.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+		.andExpect(jsonPath("$.title", equalTo("EventLite Home")))
+		.andExpect(jsonPath("$._self", equalTo("http://localhost/")))
+		.andExpect(jsonPath("$.events", equalTo("http://localhost/events")))
+		.andExpect(jsonPath("$.venues", equalTo("http://localhost/venues")));
 	}
+
 }
