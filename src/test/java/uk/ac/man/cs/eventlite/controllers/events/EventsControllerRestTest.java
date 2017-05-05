f38d5d7a6eef1a6dadedad1263ea0f7a8028bdfb
Lucian-Iulian Ghimfus <lucian-iulian.ghimfus@student.manchester.ac.uk>
2017-05-05 15:54:51 +0100

    Modified events rest controller test file to use mocking

--- src/test/java/uk/ac/man/cs/eventlite/controllers/events/EventsControllerRestTest.java
+++ src/test/java/uk/ac/man/cs/eventlite/controllers/events/EventsControllerRestTest.java
@@ -13,11 +13,13 @@ import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import uk.ac.man.cs.eventlite.TestParent;
 import uk.ac.man.cs.eventlite.controllers.EventsControllerWeb;
@@ -34,8 +36,8 @@ public class EventsControllerRestTest extends TestParent {
 
 	private MockMvc mvc;
 	
-	@InjectMocks
-	private EventsControllerWeb eventsController;
+	@Autowired
+	private WebApplicationContext ctx;
 	
 	@Mock
 	private Event event;
@@ -49,46 +51,46 @@ public class EventsControllerRestTest extends TestParent {
 	@Before
 	public void setup() {
 		MockitoAnnotations.initMocks(this);
-		mvc = MockMvcBuilders.standaloneSetup(eventsController).build();		
+		mvc = MockMvcBuilders.webAppContextSetup(ctx).build();		
 		EventTestHelper.init(venueService);
 	}
 	
-//	@Test
-//	public void testGetAllEvents() throws Exception {
-//		mvc.perform(get("/events").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
-// 		.andExpect(content().contentType(MediaType.APPLICATION_JSON))
-// 		.andExpect(jsonPath("$.title", equalTo("EventLite Events")))
-// 		.andExpect(jsonPath("$._self", equalTo("http://localhost/events")));
-//	}
+	@Test
+	public void testGetAllEvents() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/events").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+ 		.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+ 		.andExpect(jsonPath("$.title", equalTo("EventLite Events")))
+ 		.andExpect(jsonPath("$._self", equalTo("http://localhost/events")));
+	}
 
 	@Test
 	public void testGetOneEvent() throws Exception {
-		when(eventService.findById(1)).thenReturn(EventTestHelper.newEvent("Event 1", "25/3/2018", "12:00", "Short description"));
-		mvc.perform(MockMvcRequestBuilders.get("/events/1").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
-//			.andExpect(content().contentType(MediaType.APPLICATION_JSON))
-//			.andExpect(jsonPath("$.title", equalTo("EventLite event view")))
-//			.andExpect(jsonPath("$._self", equalTo("http://localhost/events/1")))
-//			.andExpect(jsonPath("$.id", equalTo("1")))
-//			.andExpect(jsonPath("$.name", equalTo("Event 1")));
+		when(eventService.findById(1)).thenReturn(event);
+		when(event.getId()).thenReturn(Long.valueOf("1"));
+		mvc.perform(MockMvcRequestBuilders.get("/events/1").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+			.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+			.andExpect(jsonPath("$.title", equalTo("EventLite event view")))
+			.andExpect(jsonPath("$._self", equalTo("http://localhost/events/1")))
+			.andExpect(jsonPath("$.id", equalTo("1")));
 	}
 	
-//	
-//	@Test
-//	public void deleteEvent() throws Exception{
-//		mvc.perform(MockMvcRequestBuilders.delete("/events/1")).andExpect(status().isNoContent());
-//	}
-//	
-//	@Test
-//	public void filterEvents() throws Exception{
-//		mvc.perform(MockMvcRequestBuilders.post("/events/").contentType(MediaType.APPLICATION_JSON)
-//				.content("{\"name\": \"\"}").accept(MediaType.APPLICATION_JSON))
-//		.andExpect(status().isOk())
-//		.andExpect(content().string(containsString("")));		
-//	}
-//
-//	@Ignore
-//	@Test
-//	public void testGetFirstEvent() throws Exception {
-//		mvc.perform(get("/events/1").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
-//	}
+	
+	@Test
+	public void deleteEvent() throws Exception{
+		mvc.perform(MockMvcRequestBuilders.delete("/events/1")).andExpect(status().isNoContent());
+	}
+	
+	@Test
+	public void filterEvents() throws Exception{
+		mvc.perform(MockMvcRequestBuilders.post("/events/").contentType(MediaType.APPLICATION_JSON)
+				.content("{\"name\": \"\"}").accept(MediaType.APPLICATION_JSON))
+		.andExpect(status().isOk())
+		.andExpect(content().string(containsString("")));		
+	}
+
+	@Ignore
+	@Test
+	public void testGetFirstEvent() throws Exception {
+		mvc.perform(get("/events/1").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
+	}
 }
