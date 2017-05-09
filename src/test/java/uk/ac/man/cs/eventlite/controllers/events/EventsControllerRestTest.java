eb6e47651c440f9f6883245078126d1fca5d520a
Kaspar Matas <kaspar.matas@student.manchester.ac.uk>
2017-05-09 03:38:05 +0300

    Improved the unit test for the user event page rest request
    Changed the unit test so that more specific information would be present
    at the json returned.

--- src/test/java/uk/ac/man/cs/eventlite/controllers/events/EventsControllerRestTest.java
+++ src/test/java/uk/ac/man/cs/eventlite/controllers/events/EventsControllerRestTest.java
@@ -6,6 +6,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.hasItem;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -65,11 +66,27 @@ public class EventsControllerRestTest extends TestParent {
 		
 	@Test
 	public void testGetUserEventsJson() throws Exception {
+		venue = new Venue("Test Venue Name", 10, null, null, null, null, null);
+		venueService.save(venue);
+		
+		event = EventTestHelper.newEvent("EventLite Event 1", venue, "25/3/2018", "12:00", "some description");
+		eventService.save(event);
+		
+		long eventId = eventService.searchByName("EventLite Event 1").get(0).getId();
+		long venueId = venueService.searchByName("Test Venue Name").iterator().next().getId();
+		
 		mvc.perform(get("/events/userevents").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
 		.andExpect(content().contentType(MediaType.APPLICATION_JSON))
 		.andExpect(jsonPath("$.title", equalTo("EventLite User Events")))
 		.andExpect(jsonPath("$._self", equalTo("http://localhost/events/userevents")))
-		.andExpect(jsonPath("$.events", notNullValue()));
+		.andExpect(jsonPath("$.events[*].id", hasItem("" + eventId)))
+		.andExpect(jsonPath("$.events[*].name", hasItem("EventLite Event 1")))
+		.andExpect(jsonPath("$.events[*]._self", hasItem("http://localhost/events/" + eventId)))
+		.andExpect(jsonPath("$.events[*].venue.id", hasItem("" + venueId)))
+		.andExpect(jsonPath("$.events[*].venue.name", hasItem("Test Venue Name")))
+		.andExpect(jsonPath("$.events[*].venue.capacity", hasItem("10")))
+		.andExpect(jsonPath("$.events[*].venue._self", hasItem("http://localhost/venues/" + venueId)));
+		
 	}
 
 }
